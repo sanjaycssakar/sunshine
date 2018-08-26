f365a15b3fcb61a4b4712bb7e2fcaa1fcf1810e1
deepak c s <sanjaycsankar98@gmail.com>
2018-08-26 23:12:29 +0530

    first commit

---
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -0,0 +1,51 @@
+package com.example.android.sunshine;
+
+import android.content.Intent;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+    private static final String TAG = "DetailActivity";
+TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        textView = (TextView) findViewById(R.id.textView);
+textView.setText(getIntent().getStringExtra("name"));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.share, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if ( id == R.id.Share){
+            Log.d(TAG, "onOptionsItemSelected: Accessed");
+            onclickshare();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    public void onclickshare(){
+        Log.d(TAG, "onclickshare: accessed");
+        String mtype = "text/plain";
+        String title = "Share weather details";
+        String textToShare =  getIntent().getStringExtra("name");
+        ShareCompat.IntentBuilder.from(this).setChooserTitle(title).setType(mtype).setText(textToShare).startChooser();
+    }
+}
